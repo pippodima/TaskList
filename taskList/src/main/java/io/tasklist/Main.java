2801457a085881be0524401071ec413452fa00a3
pippodima <pippodima99@gmail.com>
2025-08-27 19:08:00 +0200

    docker working

---
+++ taskList/src/main/java/io/tasklist/Main.java
@@ -0,0 +1,8 @@
+package io.tasklist;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("TaskList app started!");
+        // You can initialize your app here, e.g. start CLI, Spring, etc.
+    }
+}
